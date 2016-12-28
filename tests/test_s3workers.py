f4d5d1f46fa22cf9cba6a3f61bf6f285135800ee
bradrf <brad@gigglewax.com>
2016-12-28 14:47:02 -0800

    initial skeleton

---
+++ tests/test_s3workers.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_s3workers
+----------------------------------
+
+Tests for `s3workers` module.
+"""
+
+import pytest
+
+from contextlib import contextmanager
+from click.testing import CliRunner
+
+from s3workers import s3workers
+from s3workers import cli
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_command_line_interface():
+    runner = CliRunner()
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 's3workers.cli.main' in result.output
+    help_result = runner.invoke(cli.main, ['--help'])
+    assert help_result.exit_code == 0
+    assert '--help  Show this message and exit.' in help_result.output
