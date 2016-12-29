0c4f8f41877ca2eddf4d1b2f6d3e187d344eae24
bradrf <brad@gigglewax.com>
2016-12-28 20:00:18 -0800

    start docs and fix silly test

--- s3workers/__init__.py
+++ s3workers/__init__.py
@@ -1,5 +1,7 @@
-# -*- coding: utf-8 -*-
-
-__author__ = """Brad Robel-Forrest"""
+__author__ = 'Brad Robel-Forrest'
 __email__ = 'brad@bitpony.com'
 __version__ = '0.1.0'
+
+from .worker import Worker
+from .progress import S3KeyProgress
+from .jobs import S3ListJob
--- tests/test_s3workers.py
+++ tests/test_s3workers.py
@@ -1,41 +1,26 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-"""
+'''
 test_s3workers
 ----------------------------------
 
 Tests for `s3workers` module.
-"""
+'''
 
 import pytest
 
 from contextlib import contextmanager
 from click.testing import CliRunner
 
-from s3workers import s3workers
-from s3workers import cli
+import s3workers
+from s3workers.cli import main
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
 def test_command_line_interface():
     runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 's3workers.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    result = runner.invoke(main)
+    assert result.exit_code == 2 # requires a command
+    assert 'S3_URI' in result.output
+    help_result = runner.invoke(main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
